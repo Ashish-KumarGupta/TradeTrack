97010be7a9bf7739a5ff2b14e1d0178c53c9189e
Ashish Kumar Gupta <121687062+Ashish-KumarGupta@users.noreply.github.com>
2025-08-07 17:58:47 +0530

    Update Positions.js

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 
-// import { positions } from "../data/data";
+
 
 const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3002/allPositions").then((res) => {
-      // console.log(res.data);
+    axios.get("https://tradetrack-backend-mwlk.onrender.com").then((res) => {
+      
       setAllPositions(res.data);
     });
   }, []);
